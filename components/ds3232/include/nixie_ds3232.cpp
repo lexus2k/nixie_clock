f65dc1f38666f6bc063231a1e10877803301167b
User <alexey.dynda@gmail.com>
2018-10-04 09:39:36 +1000

    DS3232

---
+++ components/ds3232/include/nixie_ds3232.cpp
@@ -0,0 +1,240 @@
+/*
+    Copyright (C) 2016 Alexey Dynda
+
+    This file is part of Nixie Library.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "nixie_ds3232.h"
+#include "nixieos.h"
+
+#if !defined(__AVR_ATtiny25__) && \
+    !defined(__AVR_ATtiny45__) && \
+    !defined(__AVR_ATtiny85__)
+
+#include <Wire.h>
+
+bool Ds3231::init()
+{
+    /* Init DS3232/DS3231 */
+    Wire.beginTransmission(I2C_ADDR_DS3231);
+    Wire.write(0x0E); // Write to control register
+    Wire.write(B00011100);
+    Wire.write(B00110000);
+    if ( Wire.endTransmission() != 0)
+    {
+        m_no_device = true;
+        m_seconds     = 0x00;
+        m_minutes     = 0x00;
+        m_hours       = 0x00;
+        m_day         = 0x01;
+        m_month       = 0x01;
+        m_year        = 0x17;
+        m_day_of_week = getDayOfWeek(toDecimal(m_year) + 2000, toDecimal(m_month), toDecimal(m_day)) + 1;
+    }
+    else
+    {
+        getDateTime();
+    }
+    return !m_no_device;
+}
+
+Ds3231& Ds3231::operator  ++() 
+{
+    if (++m_seconds > 59)
+    {
+        m_seconds -= 60;
+        if (++m_minutes > 59)
+        {
+            m_minutes -= 60;
+            if (++m_hours > 23) m_hours -= 24;
+        }
+    }
+    return *this;
+}
+
+
+void Ds3231::getDateTime()
+{
+    if (m_no_device)
+    {
+        return;
+    }
+    Wire.beginTransmission(I2C_ADDR_DS3231);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    Wire.requestFrom(I2C_ADDR_DS3231, 7);
+  
+    if (Wire.available())
+    {
+        m_seconds     = Wire.read();
+        m_minutes     = Wire.read();
+        m_hours       = Wire.read();
+        m_day_of_week = Wire.read();
+        m_day         = Wire.read();
+        m_month       = Wire.read() & 0x1F;
+        m_year        = Wire.read();
+        m_lastRefreshTs = g_nixieMs;
+    }
+}
+
+void Ds3231::setDateTime()
+{
+    if (m_no_device)
+    {
+        return;
+    }
+    Wire.beginTransmission(I2C_ADDR_DS3231);
+    Wire.write(0x00);
+    Wire.write(m_seconds);
+    Wire.write(m_minutes);
+    Wire.write(m_hours);
+    Wire.write(m_day_of_week);
+    Wire.write(m_day);
+    Wire.write(m_month);
+    Wire.write(m_year);
+    Wire.endTransmission();
+    m_lastRefreshTs = g_nixieMs;
+}
+
+void    Ds3231::setDate()
+{
+    if (m_no_device)
+    {
+        return;
+    }
+    Wire.beginTransmission(I2C_ADDR_DS3231);
+    Wire.write(0x03);
+    Wire.write(m_day_of_week);
+    Wire.write(m_day);
+    Wire.write(m_month);
+    Wire.write(m_year);
+    Wire.endTransmission();
+}
+
+void    Ds3231::setTime()
+{
+    if (m_no_device)
+    {
+        return;
+    }
+    Wire.beginTransmission(I2C_ADDR_DS3231);
+    Wire.write(0x00);
+    Wire.write(m_seconds);
+    Wire.write(m_minutes);
+    Wire.write(m_hours);
+    Wire.endTransmission();
+    m_lastRefreshTs = g_nixieMs;
+}
+
+int16_t Ds3231::getTemp()
+{
+    if (m_no_device)
+    {
+        return 0;
+    }
+    //temp registers (11h-12h) get updated automatically every 64s
+    Wire.beginTransmission(I2C_ADDR_DS3231);
+    Wire.write(0x11);
+    Wire.endTransmission();
+    Wire.requestFrom(I2C_ADDR_DS3231, 2);
+    if(Wire.available())
+    {
+        uint8_t temp = Wire.read();
+        return (((temp & 0x7F) << 2) | (Wire.read() >> 6)) * ((temp & 0x80) ? -1: 1);
+    }
+    else
+        return 0;
+}
+
+uint8_t Ds3231::toInternal(uint8_t d)
+{
+    if (d > 127)
+    {
+        return 0xFF;
+    }
+    return ((d / 10) << 4) | (d % 10);
+}
+
+uint8_t Ds3231::toDecimal(uint8_t i)
+{
+    return (i & 0x0F) + (i>>4) * 10;
+};
+
+int16_t Ds3231::toMinutes(uint8_t hours, uint8_t minutes, bool decimal)
+{
+    if (!decimal)
+    {
+         hours = toDecimal(hours);
+         minutes = toDecimal(minutes);
+    }
+    return ((int16_t)hours)*60 + (int16_t)minutes;
+}
+
+int16_t Ds3231::timeDelta(int16_t min1, int16_t min2)
+{
+    int16_t delta1 = min2 - min1;
+    int16_t delta2;
+    if (delta1 < 0)
+    {
+        delta1 = - delta1;
+    }
+    delta2 = 24*60 - delta1;
+    return delta1 < delta2 ? delta1: delta2;
+}
+
+
+void Ds3231::refreshTime(ERefreshType type)
+{
+    if ( DS3231_REFRESH_FORCED == type )
+    {
+        getDateTime();
+        return;
+    }
+    /* Updating RTC to reduce requests to RTC chip */
+    if ((uint16_t)(g_nixieMs - m_lastRefreshTs) > 1000)
+    {
+        if ((uint16_t)(g_nixieMs - m_lastRefreshTs) > 3000)
+        {
+            getDateTime();
+        }
+        else
+        {
+            uint8_t seconds = Ds3231::toDecimal(m_seconds) + 1;
+            if (seconds > 59)
+            {
+                getDateTime();
+            }
+            else
+                m_seconds = Ds3231::toInternal(seconds);
+            m_lastRefreshTs += 1000;
+        }
+    }
+}
+
+#endif
+
+uint8_t getDayOfWeek(uint16_t year, uint8_t month, uint8_t day)
+{
+    uint8_t a = (14 - month) / 12;
+    uint16_t y = year - a;
+    uint8_t m = month + 12 * a - 2;
+    uint8_t dayOfWeek = (7000 + day + y + y / 4 - y / 100 + y / 400 + (31 * (uint16_t)m) / 12) % 7;
+}
+
+
+
+
+
---
+++ components/ds3232/include/nixie_ds3232.h
@@ -0,0 +1,159 @@
+/*
+    Copyright (C) 2016-2017 Alexey Dynda
+
+    This file is part of Nixie Library.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _NIXIE_DS3232_H_
+#define _NIXIE_DS3232_H_
+
+#include <nixie_types.h>
+
+typedef enum
+{
+    DS3231_REFRESH_FORCED,
+    DS3231_REFRESH_REDUCED,
+} ERefreshType;
+
+/**
+ * @brief Provides access to DS3231 register values
+ *
+ * Ds3231 class supports Ds3231 and Ds3232 chips.
+ * The class encapsulates the work with RTC chip,
+ * allowing to set and read time.
+ * Class uses Wire library.
+ */
+class Ds3231
+{
+public:
+    /**
+     * All public fields are in internal DS3231 representation: XXXX YYYY
+     * where XXXX bits is high decimal digit and YYYY is low decimal digit. 
+     * 00101001 means 29 seconds (or minutes)
+     */
+    uint8_t          m_seconds;
+    /** @copydoc Ds3231::m_seconds */
+    uint8_t          m_minutes;
+    /** @copydoc Ds3231::m_seconds */
+    uint8_t          m_hours;
+    /** @copydoc Ds3231::m_seconds */
+    uint8_t          m_day_of_week;
+    /** @copydoc Ds3231::m_seconds */
+    uint8_t          m_day;
+    /** @copydoc Ds3231::m_seconds */
+    uint8_t          m_month;
+    /** @copydoc Ds3231::m_seconds */
+    uint8_t          m_year;
+
+    /**
+     * Initializes Ds3231 class state. Should be called in setup
+     * @return true if successful, otherwise false
+     */
+    bool init();
+
+    Ds3231(): m_no_device(false) {};
+
+    /**
+     * Reads date and time from the chip
+     */
+    void    getDateTime();
+
+    /**
+     * Writes date and time to the chip
+     */
+    void    setDateTime();
+
+    /**
+     * Writes only date to the chip. The time state is preserved.
+     */
+    void    setDate();
+
+    /**
+     * Writes only time to the chip. The date state is preserved.
+     */
+    void    setTime();
+
+    /**
+     * Reads time from RTC chip.
+     * Depending on selected mode can reduce number i2c reads using
+     * arduino millis() function.
+     * @param type - DS3231_REFRESH_FORCED or DS3231_REFRESH_REDUCED
+     */
+    void    refreshTime(ERefreshType type);
+    
+    /** 
+     * Returns temp in celsius times four.
+     * For example, 100 means 25.0C
+     */
+    int16_t getTemp();
+
+    /**
+     * Increments date and time variables by 1 second
+     * The function doesn't changes state of RTC chip.
+     */
+    Ds3231& operator  ++();
+
+    /**
+     * Converts integer value to internal DS3231 representation.
+     * integer value 12 will be converted to 00010010 = 0x12
+     */
+    static uint8_t toInternal(uint8_t d);
+
+    /**
+     * Converts internal value representation to decimal:
+     * internal value 0x22 will be converted to integer 22.
+     */
+    static uint8_t toDecimal(uint8_t i);
+
+    /**
+     * Returns number of minutes passed since midnight 00:00.
+     * @param decimal - if false, passed hours and minutes values will be
+     *                  converted to decimal
+     * @return number of minutes since midnight
+     */
+    static int16_t toMinutes(uint8_t hours, uint8_t minutes, bool decimal);
+
+    /**
+     * Returns minimum time delta between 2 timestamps.
+     * For example, if timestamps are 60 and 120, the function will return 60.
+     * If timestamps are 120 and 60, the function will return 60
+     * If timestamps are 22*60 and 60, the function will return 180.
+     */
+    static int16_t timeDelta(int16_t min1, int16_t min2);
+
+    /**
+     * Returns true if it is Sunday or Saturday.
+     */
+    bool isWeekEnd() { return (m_day_of_week == 1) || (m_day_of_week == 7); };
+
+private:
+    static const int I2C_ADDR_AT24C32 = 0x57;
+    static const int I2C_ADDR_DS3231  = 0x68;
+    uint16_t m_lastRefreshTs;
+    bool m_no_device;
+};
+
+/**
+ * Returns day of week (0 - sunday, 1 - monday, etc. )
+ * @param year - Full year (4-digit)
+ * @param month - month number (1-based)
+ * @param day - day number (1-based)
+ * @return day of week
+ */
+uint8_t getDayOfWeek(uint16_t year, uint8_t month, uint8_t day);
+
+#endif /* _NIXIE_RTC_H_ */
+
