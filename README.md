fadc66a955629a8dcdb7b17a2dbbc9dcfae9294e
lexus2k <alexey.dynda@gmail.com>
2018-12-10 15:28:17 +1000

    Moved to idf 3.2-beat1

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # nixie_clock
 
-> git submodule update --init --recursive
+## How to get project
 
+> git submodule update --init --recursive
 > git clone --recursive
+
+## Recommended ESP32 IDF version
+
+v3.2-beta
+
+> git clone -b v3.2-beta1 --recursive https://github.com/espressif/esp-idf.git esp-idf<br>
--- flash_factory_nvs.sh
+++ flash_factory_nvs.sh
@@ -6,13 +6,23 @@ if [ "$idx" = "" ]; then
     exit 1
 fi
 
+if [ "$2" = "old" ]; then
+${IDF_PATH}/components/nvs_flash/nvs_partition_generator/nvs_partition_gen.py \
+      devices/device-${idx}.csv device-${idx}.bin 0x6000
+else
 ${IDF_PATH}/components/nvs_flash/nvs_partition_generator/nvs_partition_gen.py \
       --input devices/device-${idx}.csv --output device-${idx}.bin --size 0x6000
+fi
+
 if [ $? -ne 0 ]; then
     echo "Failed to generate NVS partition data"
     exit 1
 fi
+if [ "$2" = "old" ]; then
+${IDF_PATH}/components/esptool_py/esptool/esptool.py --port /dev/ttyS6 write_flash 0x12000 device-${idx}.bin
+else
 ${IDF_PATH}/components/esptool_py/esptool/esptool.py --port /dev/ttyUSB0 write_flash 0x12000 device-${idx}.bin
+fi
 if [ $? -ne 0 ]; then
     echo "Failed to flash NVS partition data"
     exit 1
