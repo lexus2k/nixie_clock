bd7efda33ef7ca598182e806ed9f763cf2f05816
lexus2k <alexey.dynda@gmail.com>
2019-05-31 16:43:09 +1000

    Disable power save for wifi by default

--- components/wifi_manager/src/wifi_manager.c
+++ components/wifi_manager/src/wifi_manager.c
@@ -95,6 +95,7 @@ static bool __wifi_manager_switch_mode(wifi_mode_t mode, wifi_config_t *conf)
         {
             ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_AP, conf ) );
             err = esp_wifi_start();
+            esp_wifi_set_ps(WIFI_PS_NONE);
         }
         else if ( mode == WIFI_MODE_STA )
         {
@@ -103,6 +104,7 @@ static bool __wifi_manager_switch_mode(wifi_mode_t mode, wifi_config_t *conf)
                 ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, conf ) );
             }
             err = esp_wifi_start();
+            esp_wifi_set_ps(WIFI_PS_NONE);
         }
     }
     return err == ESP_OK;
