c5ba41b9fc2e74dc253978658045a914c60b2c6d
Aleksei Dynda <alexey.dynda@gmail.com>
2019-07-04 21:54:52 +1000

    New version 0.2.275 is released

--- main/version.h
+++ main/version.h
@@ -1,7 +1,7 @@
 #include "../build_revision"
 #include <stdlib.h>
 
-#define FW_VERSION "0.1." BUILD_REVISION
+#define FW_VERSION "0.2." BUILD_REVISION
 
 static inline bool is_version_newer(const char *version)
 {
