6491df1471716cc9d068d0dedff015707d8f568a
Aleksei Dynda <alexey.dynda@gmail.com>
2020-02-12 15:59:29 +1000

    Version file is updated

--- main/version.h
+++ main/version.h
@@ -1,7 +1,7 @@
 #include "../build_revision"
 #include <stdlib.h>
 
-#define FW_VERSION "0.3." BUILD_REVISION
+#define FW_VERSION "0.4." BUILD_REVISION
 
 static inline bool is_version_newer(const char *version)
 {
