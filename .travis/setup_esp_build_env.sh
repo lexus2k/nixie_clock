ba4138bea67ebf2765673732aaa619ec571bb0a0
Aleksei Dynda <alexey.dynda@gmail.com>
2020-03-29 19:26:58 +1000

    Moving to v3.3.1 release

--- .travis/setup_esp_build_env.sh
+++ .travis/setup_esp_build_env.sh
@@ -7,7 +7,8 @@ else
     # sudo apt-get install libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
     #
     mkdir -p ~/esp
-    git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+    git clone -b v3.3.1 --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+    # git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
 fi
 
 cd ~/esp/esp-idf
