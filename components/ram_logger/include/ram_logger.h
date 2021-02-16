3bb9b38310ce63e5809b29f58345bc9a1bf678ed
lexus2k <alexey.dynda@gmail.com>
2021-02-16 14:08:46 +1000

    Updated copyrights

--- components/ram_logger/include/ram_logger.h
+++ components/ram_logger/include/ram_logger.h
@@ -1,3 +1,27 @@
+/*
+    MIT License
+
+    Copyright (c) 2018-2020, Alexey Dynda
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
 #pragma once
 
 #ifdef __cplusplus
@@ -8,9 +32,20 @@ extern "C" {
 #include "esp_log.h"
 #include <string.h>
 
+/**
+ * Allocates buffer for the RAM logger, initializes internal variables
+ * @param ram_size ram size in bytes
+ */
 int ram_logger_init(int ram_size);
-int ram_logger_vprintf( const char *str, va_list l );
+
+/**
+ * Removes ram logger from memory and restores original logger
+ */
 void ram_logger_free(void);
+
+/**
+ * Returns pointer to the buffer (null-terminated) containing collected logs
+ */
 const char *ram_logger_get_logs(void);
 
 #ifdef __cplusplus
--- components/ram_logger/src/ram_logger.c
+++ components/ram_logger/src/ram_logger.c
@@ -1,3 +1,27 @@
+/*
+    MIT License
+
+    Copyright (c) 2018-2020, Alexey Dynda
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
 #include "ram_logger.h"
 
 #include "esp_system.h"
@@ -11,6 +35,8 @@ static vprintf_like_t old_function = NULL;
 static char *text_buffer = NULL;
 static int buffer_size = 0;
 static int position = 0;
+
+int ram_logger_vprintf( const char *str, va_list l );
 
 int ram_logger_init(int ram_size)
 {
