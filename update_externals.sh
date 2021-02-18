694da13c8b53e8b2b163174ec48577aad3586700
lexus2k <alexey.dynda@gmail.com>
2021-02-19 09:32:20 +1000

    Switching to latest libraries

--- update_externals.sh
+++ update_externals.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-git submodule foreach git pull origin master
+cd components/audio_lib && git pull origin master && cd ../..
+cd components/vgm_decoder && git pull origin master && cd ../..
+cd components/sm_engine && git pull origin master && cd ../..
+cd components/ram_logger && git pull origin main && cd ../..
+
+# git submodule foreach git pull origin master
