3df7fff32f540ddd73321cb2933ebabeba4fc528
lexus2k <alexey.dynda@gmail.com>
2021-02-19 11:12:47 +1000

    Removed wire and spibus libraries

--- components/spibus/component.mk
+++
@@ -1,5 +0,0 @@
-# This is Makefile for ESP32 IDF
-
-COMPONENT_ADD_INCLUDEDIRS := ./include
-COMPONENT_SRCDIRS := ./src
-# COMPONENT_DEPENDS
--- components/wire/component.mk
+++
@@ -1,4 +0,0 @@
-# This is Makefile for ESP32 IDF
-
-COMPONENT_ADD_INCLUDEDIRS := ./include
-COMPONENT_SRCDIRS := ./src
