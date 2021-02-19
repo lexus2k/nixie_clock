2ce159ad101b925916aeabea2f15e87a2eaa0b1d
lexus2k <alexey.dynda@gmail.com>
2021-02-19 11:14:01 +1000

    Integrated external dependencies

--- update_externals.sh
+++ update_externals.sh
@@ -4,5 +4,7 @@ cd components/audio_lib && git pull origin master && cd ../..
 cd components/vgm_decoder && git pull origin master && cd ../..
 cd components/sm_engine && git pull origin master && cd ../..
 cd components/ram_logger && git pull origin main && cd ../..
+cd components/spibus && git pull origin main && cd ../..
+cd components/wire && git pull origin main && cd ../..
 
 # git submodule foreach git pull origin master
